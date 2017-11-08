b098e932dde91a6b8f34a3db8eb6dc83d3f0158f
taho101 <tahirski@gmail.com>
2017-11-08 23:32:49 +0100

    Added basic operations and variable examples

--- demos/typesVariables/Program.cs
+++ demos/typesVariables/Program.cs
@@ -6,7 +6,37 @@ namespace typesVariables
     {
         static void Main(string[] args)
         {
+            // integer
+            int number1 = 1;
+            Console.WriteLine("Example of integer is " + number1);
+
+            // double
+            double number2 = 1.23;
+            Console.WriteLine("Example of double is " + number2);
+
+            // string
+            string text = "Hello World";
+            Console.WriteLine("Our small program says " + text);
+
+            // boolean
+            bool def = true;
+            Console.WriteLine("A true statement is always " + def);
+
+            // operations + - * /
             Console.WriteLine("2 + 2 = " + (2+2));
+            Console.WriteLine("2 - 2 = " + (2-2));
+            Console.WriteLine("2 * 2 = " + (2*2));
+            Console.WriteLine("2 / 2 = " + (2/2));
+
+            // concatenation of strings
+            Console.WriteLine("Hello" + " " + "World" + "!");
+
+            // boolean operations (&& || !)
+            Console.WriteLine("True AND True = " + (true && true));
+            Console.WriteLine("True AND False = " + (true && false));
+            Console.WriteLine("True OR False = " + (true || false));
+            Console.WriteLine("False OR False = " + (false || false));
+            Console.WriteLine("NOT True = " + (!true));
         }
     }
 }
